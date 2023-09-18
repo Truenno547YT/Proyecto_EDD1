ef8748df1164a87f80304f0fe1ae7a31204a0021
Truenno547YT <d.andres@correo.unimet.edu.ve>
2023-09-18 13:06:00 +0000

    Commit inicial

---
+++ src/proyecto_edd1/Proyecto_EDD1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_edd1;
+
+/**
+ *
+ * @author truenno
+ */
+public class Proyecto_EDD1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
